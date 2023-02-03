9ec0bf8a45ae94e08454c9a058e62b1b6436a179
Youfu Yan <arnoldyyf@gmail.com>
2023-02-03 13:58:56 -0600

    suvey.html and thankyou.html

--- server.py
+++ server.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template,  redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    if request.method == "POST":
+        customer = request.form.get('customer')
+        breeder = request.form.get('breeder')
+        rating = request.form.get('rating')
+        comments = request.form.get('comments')
+        # print(customer, breeder, rating, comments)
+        if customer == '' or breeder == '':
+            return render_template('index.html', message='Please enter required fields')
+        return render_template('thankyou.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

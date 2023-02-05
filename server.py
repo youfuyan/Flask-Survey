e02ea858d49537f5a3232812d89a6f5ed0e45a2d
Youfu Yan <arnoldyyf@gmail.com>
2023-02-04 21:19:52 -0600

    rewrite the routing

--- server.py
+++ server.py
@@ -9,17 +9,39 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/submit", methods=['POST'])
-def submit():
+@app.route('/survey', methods=['GET', 'POST'])
+def survey():
+    # if request.method == 'POST':
+    #     if not request.form.get('text_input') or len(request.form.get('text_input')) < 3:
+    #         return render_template('survey.html', error='Text input is required and must have a minimum length of 3 characters.')
+    #     else:
+    #         return render_template('thanks.html')
+    # return render_template('survey.html')
     if request.method == "POST":
         customer = request.form.get('customer')
         breeder = request.form.get('breeder')
         rating = request.form.get('rating')
         comments = request.form.get('comments')
-        # print(customer, breeder, rating, comments)
+        print(customer, breeder, rating, comments)
         if customer == '' or breeder == '':
-            return render_template('index.html', message='Please enter required fields')
-        return render_template('thankyou.html')
+            return render_template('survey.html', message='Please enter required fields')
+        else:
+            return render_template('thanks.html')
+    return render_template('survey.html')
+
+@app.route('/decline')
+def decline():
+    return render_template('decline.html')
+
+@app.route('/thanks', methods=['POST'])
+def thanks():
+    customer = request.form.get('customer')
+    breeder = request.form.get('breeder')
+    rating = request.form.get('rating')
+    comments = request.form.get('comments')
+
+    # ... process form data and render the /thanks template with the customer, breeder, rating, and comments variables
+    return render_template('thanks.html', customer=customer, breeder=breeder, rating=rating, comments=comments)
 
 
 if __name__ == "__main__":
